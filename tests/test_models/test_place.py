2206fe9f4828bb77ded72517632a1dcaf83c8f62
Marvydee <marvydee1@gmail.com>
2024-03-11 00:24:39 +0100

    place test file added

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,48 @@
+import unittest
+from models.place import Place
+
+class TestPlace(unittest.TestCase):
+    """Test cases for the Place class"""
+
+    def setUp(self):
+        """Set up a Place instance for testing"""
+        self.place = Place()
+
+    def test_attributes(self):
+        """Test if the Place instance has the correct attributes"""
+        self.assertTrue(hasattr(self.place, 'id'))
+        self.assertTrue(hasattr(self.place, 'created_at'))
+        self.assertTrue(hasattr(self.place, 'updated_at'))
+        self.assertTrue(hasattr(self.place, 'city_id'))
+        self.assertTrue(hasattr(self.place, 'user_id'))
+        self.assertTrue(hasattr(self.place, 'name'))
+        self.assertTrue(hasattr(self.place, 'description'))
+        self.assertTrue(hasattr(self.place, 'number_rooms'))
+        self.assertTrue(hasattr(self.place, 'number_bathrooms'))
+        self.assertTrue(hasattr(self.place, 'max_guest'))
+        self.assertTrue(hasattr(self.place, 'price_by_night'))
+        self.assertTrue(hasattr(self.place, 'latitude'))
+        self.assertTrue(hasattr(self.place, 'longitude'))
+        self.assertTrue(hasattr(self.place, 'amenity_ids'))
+
+    def test_inheritance(self):
+        """Test if Place inherits from BaseModel"""
+        self.assertTrue(issubclass(Place, BaseModel))
+
+    def test_attribute_types(self):
+        """Test if the attribute types are correct"""
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.user_id, str)
+        self.assertIsInstance(self.place.name, str)
+        self.assertIsInstance(self.place.description, str)
+        self.assertIsInstance(self.place.number_rooms, int)
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertIsInstance(self.place.amenity_ids, list)
+
+if __name__ == '__main__':
+    unittest.main()
+
