3f56d06c8bcd399fe7e862093458530846857765
Marvydee <marvydee1@gmail.com>
2024-03-10 23:33:21 +0100

    file_storage file added

---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import json
+import uuid
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+def all(self):
+    """return dictionary"""
+    return self.__objects
+
+def new(self, obj):
+        """Sets an object in the __objects dictionary."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+def save(self):
+        """serialize the file path to JSON file path
+        """
+        our_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(our_dict, f)
+
+def reload(self):
+        """Deserializes the JSON file to __objects."""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj_dict = value
+                    module = __import__("models." + class_name, fromlist=[class_name])
+                    class_ = getattr(module, class_name)
+                    obj = class_(**obj_dict)
+                    self.__objects[key] = obj
+
+
+
+
