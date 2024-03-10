97b740f19c28735445d07ddb81581823002fe0f8
Marvydee <marvydee1@gmail.com>
2024-03-10 23:26:22 +0100

    basemodel file error fixed

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" Class BaseModel """
 import uuid
 from datetime import datetime
 from models.engine.file_storage import storage
 
 
 class BaseModel:
+     """ construct """
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -20,16 +22,21 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.createdat = datetime.now()
             self.updatedat = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
+         """ String """
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
     
     def save(self):
+        """save function"""
         self.updatedat = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """ Return a dictonary """
         our_dict = dict(self.__dict__)
         our_dict["__class__"] = (type(self).__name__)
         our_dict['createdat'] = self.createdat.isoformat()
         our_dict['updatedat'] = self.updatedat.isoformat()
+        return our_dict
