e59b3349b6f3a5da54befc7ded6b7a2cd6250b55
Frankiln <odinakachukwufranklin0@gmail.com>
2024-03-10 18:06:25 -0700

    update test files

--- models/city.py
+++ models/city.py
@@ -2,11 +2,12 @@
 """This is the city class"""
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
-     """This is the class for City
+    """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-     state_id = ""
-     name = ""
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -2,10 +2,10 @@
 """This is the state class"""
 from models.base_model import BaseModel
 
-class State(BaseModel): 
+
+class State(BaseModel):
     """This is the class for State
     Attributes:
         name: input name
     """
     name = ""
-
